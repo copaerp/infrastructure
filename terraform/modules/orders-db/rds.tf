8ca386ad624777f5a3e5037c5633025a80ada53a
unknown <erick.pio@sptech.school>
2025-05-16 18:06:31 -0300

    fix: mudando para t3.micro

--- terraform/modules/orders-db/rds.tf
+++ terraform/modules/orders-db/rds.tf
@@ -3,7 +3,7 @@ resource "aws_db_instance" "copa_db" {
   storage_type              = "gp2"
   engine                    = "postgres"
   engine_version            = "12"                     
-  instance_class            = "db.t2.micro"                 
+  instance_class            = "db.t3.micro"                 
   db_name                   = "copadb"
   username                  = "admin"
   password                  = "#Urubu100"
