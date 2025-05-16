0a612111de531218a840794377540904a1198033
unknown <erick.pio@sptech.school>
2025-05-16 17:56:34 -0300

    fix: alterando versão do postgre para 14

--- terraform/modules/orders-db/rds.tf
+++ terraform/modules/orders-db/rds.tf
@@ -2,11 +2,12 @@ resource "aws_db_instance" "copa_db" {
   allocated_storage         = 20
   storage_type              = "gp2"
   engine                    = "postgres"
-  instance_class            = "db.t2.micro"               
+  engine_version            = "14.11"                       
+  instance_class            = "db.t2.micro"                 
   db_name                   = "copadb"
   username                  = "admin"
   password                  = "#Urubu100"
-  parameter_group_name      = "default.postgres15"        
+  parameter_group_name      = "default.postgres14"
   skip_final_snapshot       = true
   publicly_accessible       = false
   multi_az                  = false
