0967b7c007cd7c0e80cec283174f3530bc393060
unknown <erick.pio@sptech.school>
2025-05-17 01:05:51 -0300

    mudando identificador do banco de dados

--- terraform/modules/orders-db/rds.tf
+++ terraform/modules/orders-db/rds.tf
@@ -1,10 +1,11 @@
 resource "aws_db_instance" "copa_db" {
+  identifier              = "copa-db-instance"
   allocated_storage       = 10
   engine                  = "mysql"
   engine_version          = "8.0.35"
   instance_class          = "db.t3.micro"
   username                = "copa_admin"
-  password                = "#Urubu100"
+  password                = var.db_password
   db_name                 = "copadb"
   skip_final_snapshot     = true
 
