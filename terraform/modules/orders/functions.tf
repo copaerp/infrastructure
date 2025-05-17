77071151a787dbaf810cf532837e4b7bee45d704
joaqu1m <joaqu1m.pires@hotmail.com>
2025-05-17 03:34:38 +0000

    env vars config

--- terraform/modules/orders/functions.tf
+++ terraform/modules/orders/functions.tf
@@ -3,6 +3,11 @@ locals {
   lambda_runtime       = "provided.al2023"
   lambda_architectures = ["arm64"]
   dummy_source_file    = "${path.root}/dummy_bootstrap.zip"
+  envs = {
+    n8n_webhook_url       = "https://n8n.copaerp.site/webhook/aba98742-debe-4f62-a283-55519635318b"
+    environment           = "prod"
+    whatsapp_verify_token = "your_verify_token"
+  }
 }
 
 data "aws_iam_role" "existing_role" {
@@ -19,6 +24,10 @@ resource "aws_lambda_function" "channel_dispatcher" {
 
   architectures = local.lambda_architectures
 
+  environment {
+    variables = local.envs
+  }
+
   timeout     = 30
   memory_size = 128
 }
@@ -33,6 +42,10 @@ resource "aws_lambda_function" "message_standardizer" {
 
   architectures = local.lambda_architectures
 
+  environment {
+    variables = local.envs
+  }
+
   timeout     = 30
   memory_size = 128
 }
@@ -46,6 +59,10 @@ resource "aws_lambda_function" "frontend_bridge" {
   filename = local.dummy_source_file
 
   architectures = local.lambda_architectures
+
+  environment {
+    variables = local.envs
+  }
 
   timeout     = 30
   memory_size = 128
