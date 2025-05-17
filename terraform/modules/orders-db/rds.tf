87c74232ac83ef9fdee8f4b8614840ee5f60b2c4
unknown <erick.pio@sptech.school>
2025-05-17 00:47:04 -0300

    migração para mysql

--- terraform/modules/orders-db/rds.tf
+++ terraform/modules/orders-db/rds.tf
@@ -1,20 +1,38 @@
-resource "aws_db_instance" "copa_db" {
-  allocated_storage       = 20
-  storage_type            = "gp2"
-  engine                  = "postgres"
-  engine_version          = "12"
-  instance_class          = "db.t3.micro"
-  db_name                 = "copadb"
+resource "aws_db_instance" "copa_db"{
+  allocated_storage       = 10
+  engine                  = "mysql"
+  engine_version          = "5.7"
+  instance_class          = "db.t2.micro"
+  Name                    = "copa_db"
   username                = "copa_admin"
   password                = "#Urubu100"
   skip_final_snapshot     = true
-  publicly_accessible     = false
-  multi_az                = false
-  backup_retention_period = 7
-  deletion_protection     = false
+  
 
-  tags = {
+tags = {
     Name        = "copadb"
     Environment = "Dev"
   }
 }
+
+
+# resource "aws_db_instance" "copa_db" {
+#   allocated_storage       = 20
+#   storage_type            = "gp2"
+#   engine                  = "postgres"
+#   engine_version          = "12"
+#   instance_class          = "db.t3.micro"
+#   db_name                 = "copadb"
+#   username                = "copa_admin"
+#   password                = "#Urubu100"
+#   skip_final_snapshot     = true
+#   publicly_accessible     = false
+#   multi_az                = false
+#   backup_retention_period = 7
+#   deletion_protection     = false
+
+#   tags = {
+#     Name        = "copadb"
+#     Environment = "Dev"
+#   }
+# }
