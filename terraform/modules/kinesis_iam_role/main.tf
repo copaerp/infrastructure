c1c9113953db3d0611eaa0aeb69fba4f28d5ff7a
unknown <erick.pio@sptech.school>
2025-05-26 00:50:24 -0300

    subindo s3-bronze

--- terraform/modules/kinesis_iam_role/main.tf
+++ terraform/modules/kinesis_iam_role/main.tf
@@ -16,24 +16,24 @@ resource "aws_iam_role" "this" {
   })
 }
 
-resource "aws_iam_role_policy" "this" {
-  name   = "${var.role_name}-policy"
-  role   = aws_iam_role.this.id
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "s3:PutObject",
-          "s3:GetBucketLocation",
-          "s3:ListBucket"
-        ],
-        Resource = [
-          var.bucket_arn,
-          "${var.bucket_arn}/*"
-        ]
-      }
-    ]
-  })
-}
+# resource "aws_iam_role_policy" "this" {
+#   name   = "${var.role_name}-policy"
+#   role   = aws_iam_role.this.id
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Effect = "Allow",
+#         Action = [
+#           "s3:PutObject",
+#           "s3:GetBucketLocation",
+#           "s3:ListBucket"
+#         ],
+#         Resource = [
+#           var.bucket_arn,
+#           "${var.bucket_arn}/*"
+#         ]
+#       }
+#     ]
+#   })
+# }
