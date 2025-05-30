894a19ba6154cf8605de448630e6850912b71e7f
joaqu1m <joaqu1m.pires@hotmail.com>
2025-05-30 23:09:06 +0000

    removing kinesis role temporarily

--- terraform/modules/data/kinesis_role.tf
+++ terraform/modules/data/kinesis_role.tf
@@ -1,39 +1,39 @@
-resource "aws_iam_role" "this" {
-  name = "kinesis-role"
+# resource "aws_iam_role" "this" {
+#   name = "kinesis-role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Principal = {
-          Service = "firehose.amazonaws.com"
-        },
-        Effect = "Allow",
-        Sid    = ""
-      }
-    ]
-  })
-}
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRole",
+#         Principal = {
+#           Service = "firehose.amazonaws.com"
+#         },
+#         Effect = "Allow",
+#         Sid    = ""
+#       }
+#     ]
+#   })
+# }
 
-resource "aws_iam_role_policy" "this" {
-  role = aws_iam_role.this.id
+# resource "aws_iam_role_policy" "this" {
+#   role = aws_iam_role.this.id
 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "s3:PutObject",
-          "s3:GetBucketLocation",
-          "s3:ListBucket"
-        ],
-        Resource = [
-          aws_s3_bucket.general_bronze.arn,
-          "${aws_s3_bucket.general_bronze.arn}/*"
-        ]
-      }
-    ]
-  })
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Effect = "Allow",
+#         Action = [
+#           "s3:PutObject",
+#           "s3:GetBucketLocation",
+#           "s3:ListBucket"
+#         ],
+#         Resource = [
+#           aws_s3_bucket.general_bronze.arn,
+#           "${aws_s3_bucket.general_bronze.arn}/*"
+#         ]
+#       }
+#     ]
+#   })
+# }
