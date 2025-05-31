1b144eca33fd05b78f47129e35f75807d2e60b03
joaqu1m <joaqu1m.pires@hotmail.com>
2025-05-31 00:08:48 +0000

    dropping database

--- terraform/modules/orders-db/rds.tf
+++ terraform/modules/orders-db/rds.tf
@@ -1,36 +1,36 @@
-resource "aws_db_instance" "orders_db" {
-  identifier        = "orders-db"
-  allocated_storage = 20
-  engine            = "mysql"
-  engine_version    = "8.0.35"
-  instance_class    = "db.t3.micro"
+# resource "aws_db_instance" "orders_db" {
+#   identifier        = "orders-db"
+#   allocated_storage = 20
+#   engine            = "mysql"
+#   engine_version    = "8.0.35"
+#   instance_class    = "db.t3.micro"
 
-  db_name  = "orders"
-  username = "admin"
-  password = "#Urubu100"
+#   db_name  = "orders"
+#   username = "admin"
+#   password = "#Urubu100"
 
-  skip_final_snapshot = true
-  publicly_accessible = true
+#   skip_final_snapshot = true
+#   publicly_accessible = true
 
-  iam_database_authentication_enabled = true
+#   iam_database_authentication_enabled = true
 
-  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+#   vpc_security_group_ids = [aws_security_group.rds_sg.id]
 
-  tags = {
-    Name        = "orders-db"
-    Environment = "production"
-  }
-}
+#   tags = {
+#     Name        = "orders-db"
+#     Environment = "production"
+#   }
+# }
 
 output "orders_db_username" {
-  value = aws_db_instance.orders_db.username
+  value = "aws_db_instance.orders_db.username"
 }
 output "orders_db_password" {
-  value = aws_db_instance.orders_db.password
+  value = "aws_db_instance.orders_db.password"
 }
 output "orders_db_endpoint" {
-  value = aws_db_instance.orders_db.endpoint
+  value = "aws_db_instance.orders_db.endpoint"
 }
 output "orders_db_name" {
-  value = aws_db_instance.orders_db.db_name
+  value = "aws_db_instance.orders_db.db_name"
 }
