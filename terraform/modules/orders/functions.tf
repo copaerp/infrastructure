d601c26d86036f1f3c179bd5851a6e7cf4dac793
joaqu1m <joaqu1m.pires@hotmail.com>
2025-05-31 21:55:29 +0000

    returning with lambda sg

--- terraform/modules/orders/functions.tf
+++ terraform/modules/orders/functions.tf
@@ -15,6 +15,28 @@ locals {
     orders_db_name          = var.orders_db_name
   }
 }
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_security_group" "lambda_sg" {
+  name   = "lambda-sg"
+  vpc_id = data.aws_vpc.default.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 resource "aws_lambda_function" "channel_dispatcher" {
   function_name = "channel-dispatcher"
