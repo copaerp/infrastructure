2528e0dfb0a0c734662e3035efc4fb099d09026a
joaqu1m <joaqu1m.pires@hotmail.com>
2025-06-08 17:44:30 +0000

    order timeout workflow id

--- terraform/modules/orders/functions.tf
+++ terraform/modules/orders/functions.tf
@@ -4,17 +4,18 @@ locals {
   lambda_architectures = ["arm64"]
   dummy_source_file    = "${path.root}/dummy_bootstrap.zip"
   envs = {
-    whatsapp_api_url        = "https://graph.facebook.com/v22.0/"
-    n8n_webhook_url         = "https://n8n.copaerp.site/webhook/"
-    new_message_workflow_id = "998ea582-5067-4362-b677-96c6f9991a7f"
-    environment             = "prod"
-    whatsapp_verify_token   = "your_verify_token"
-    orders_db_username      = var.orders_db_username
-    orders_db_password      = var.orders_db_password
-    orders_db_endpoint      = var.orders_db_endpoint
-    orders_db_name          = var.orders_db_name
-    role_arn                = var.iam_role_id
-    api_destination_arn     = aws_eventbridge_api_destination.order_api_destination.arn
+    whatsapp_api_url          = "https://graph.facebook.com/v22.0/"
+    n8n_webhook_url           = "https://n8n.copaerp.site/webhook/"
+    new_message_workflow_id   = "bf32139d-ca1f-4d01-bc45-9d4809368fa9"
+    order_timeout_workflow_id = "0e0f95f5-c30b-43e8-b52d-65dc1a1684ee"
+    environment               = "prod"
+    whatsapp_verify_token     = "your_verify_token"
+    orders_db_username        = var.orders_db_username
+    orders_db_password        = var.orders_db_password
+    orders_db_endpoint        = var.orders_db_endpoint
+    orders_db_name            = var.orders_db_name
+    role_arn                  = var.iam_role_id
+    api_destination_arn       = aws_eventbridge_api_destination.order_api_destination.arn
   }
 
   ot_envs = {
