cf581baa2990f2214ff296189b8d3bcb86166b4e
joaqu1m <joaqu1m.pires@hotmail.com>
2025-08-30 22:55:14 +0000

    removing all tf fargate config

--- terraform/modules/orders/ui.tf
+++ terraform/modules/orders/ui.tf
@@ -68,11 +68,6 @@ resource "aws_ecs_task_definition" "nginx" {
           protocol      = "tcp"
         }
       ]
-      command = [
-        "/bin/sh",
-        "-c",
-        "aws s3 sync s3://${aws_s3_bucket.frontend.bucket} /usr/share/nginx/html && nginx -g 'daemon off;'"
-      ]
     }
   ])
 }
