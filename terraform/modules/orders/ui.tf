baa662dec8020c8d404b310389d5888138637262
joaqu1m <joaqu1m.pires@hotmail.com>
2025-08-31 23:31:26 +0000

    abandoning fargate

--- terraform/modules/orders/ui.tf
+++ terraform/modules/orders/ui.tf
@@ -1,6 +1,9 @@
 variable "domain_name" {
   default = "orders.copaerp.site"
 }
+variable "zone_id" {
+  default = var.route53_main_zone_id
+}
 
 # ----------------------------
 # S3 Bucket (arquivos buildados)
@@ -9,72 +12,53 @@ resource "aws_s3_bucket" "frontend" {
   bucket = "copaerp-orders-ui-bucket"
 }
 
-# ----------------------------
-# ACM Certificate
-# ----------------------------
-resource "aws_acm_certificate" "cert" {
-  domain_name       = var.domain_name
-  validation_method = "DNS"
+# Cluster ECS
+resource "aws_ecs_cluster" "orders" {
+  name = "orders-cluster"
 }
 
-resource "aws_route53_record" "cert_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      type   = dvo.resource_record_type
-      record = dvo.resource_record_value
-    }
-  }
-
-  zone_id = var.route53_main_zone_id
-  name    = each.value.name
-  type    = each.value.type
-  records = [each.value.record]
-  ttl     = 60
-}
-
-resource "aws_acm_certificate_validation" "cert" {
-  certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
-}
-
-# ----------------------------
-# ECS Cluster
-# ----------------------------
-resource "aws_ecs_cluster" "this" {
-  name = "orders-ui-cluster"
-}
-
-# ----------------------------
-# ECS Task Definition (NGINX)
-# ----------------------------
-resource "aws_ecs_task_definition" "nginx" {
-  family                   = "orders-ui-task"
+# Task Definition
+resource "aws_ecs_task_definition" "orders_ui" {
+  family                   = "orders-ui"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
   cpu                      = "256"
   memory                   = "512"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  execution_role_arn       = var.iam_role_id
+  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
 
-  container_definitions = jsonencode([
-    {
-      name      = "nginx"
-      image     = "${var.account_id}.dkr.ecr.us-east-1.amazonaws.com/orders-ui-nginx-s3:latest"
-      essential = true
-      portMappings = [
-        {
-          containerPort = 80
-          hostPort      = 80
-          protocol      = "tcp"
-        }
-      ]
-    }
-  ])
+  container_definitions = jsonencode([{
+    name  = "orders-ui"
+    image = "nginx:latest"
+    portMappings = [{
+      containerPort = 80
+      hostPort      = 80
+    }]
+  }])
 }
 
-# ----------------------------
-# VPC + Networking
-# ----------------------------
+# Service ECS (usa default VPC/subnets automaticamente)
+resource "aws_ecs_service" "orders_ui" {
+  name            = "orders-ui"
+  cluster         = aws_ecs_cluster.orders.id
+  task_definition = aws_ecs_task_definition.orders_ui.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    assign_public_ip = true
+    # se não informar subnets, Terraform vai reclamar
+    # então você pode buscar as subnets default com data sources
+    subnets = data.aws_subnets.default.ids
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.orders_ui.arn
+    container_name   = "orders-ui"
+    container_port   = 80
+  }
+}
+
+# Descobrir subnets da VPC default
 data "aws_vpc" "default" {
   default = true
 }
@@ -86,134 +70,41 @@ data "aws_subnets" "default" {
   }
 }
 
-resource "aws_security_group" "alb_sg" {
-  vpc_id = data.aws_vpc.default.id
-  name   = "orders-ui-alb-sg"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "ecs_sg" {
-  vpc_id = data.aws_vpc.default.id
-  name   = "orders-ui-ecs-sg"
-
-  ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb_sg.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# ----------------------------
 # ALB
-# ----------------------------
-resource "aws_lb" "this" {
-  name               = "orders-ui-alb"
-  internal           = false
+resource "aws_lb" "orders_ui" {
+  name               = "orders-ui"
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
   subnets            = data.aws_subnets.default.ids
 }
 
-resource "aws_lb_target_group" "this" {
-  name        = "orders-ui-tg"
+resource "aws_lb_target_group" "orders_ui" {
+  name        = "orders-ui"
   port        = 80
   protocol    = "HTTP"
   vpc_id      = data.aws_vpc.default.id
   target_type = "ip"
 }
 
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.this.arn
+resource "aws_lb_listener" "orders_ui" {
+  load_balancer_arn = aws_lb.orders_ui.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
-    type = "redirect"
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
-}
-
-resource "aws_lb_listener" "https" {
-  load_balancer_arn = aws_lb.this.arn
-  port              = 443
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = aws_acm_certificate_validation.cert.certificate_arn
-
-  default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.this.arn
+    target_group_arn = aws_lb_target_group.orders_ui.arn
   }
 }
 
-# ----------------------------
-# ECS Service
-# ----------------------------
-resource "aws_ecs_service" "nginx" {
-  name            = "orders-ui-svc"
-  cluster         = aws_ecs_cluster.this.id
-  task_definition = aws_ecs_task_definition.nginx.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-
-  network_configuration {
-    subnets          = data.aws_subnets.default.ids
-    security_groups  = [aws_security_group.ecs_sg.id]
-    assign_public_ip = true
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.this.arn
-    container_name   = "nginx"
-    container_port   = 80
-  }
-
-  depends_on = [aws_lb_listener.https]
-}
-
-# ----------------------------
-# Route53 record
-# ----------------------------
-resource "aws_route53_record" "frontend_alias" {
-  zone_id = var.route53_main_zone_id
+# DNS
+resource "aws_route53_record" "orders_ui" {
+  zone_id = var.zone_id
   name    = var.domain_name
   type    = "A"
 
   alias {
-    name                   = aws_lb.this.dns_name
-    zone_id                = aws_lb.this.zone_id
-    evaluate_target_health = false
+    name                   = aws_lb.orders_ui.dns_name
+    zone_id                = aws_lb.orders_ui.zone_id
+    evaluate_target_health = true
   }
 }
