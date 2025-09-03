023b648c746276eed45d15004d0b9af4bdf55c40
joaqu1m <joaqu1m.pires@hotmail.com>
2025-09-03 22:00:47 +0000

    pure s3 config testing

--- terraform/modules/orders/ui.tf
+++ terraform/modules/orders/ui.tf
@@ -1,107 +1,61 @@
-# variable "domain_name" {
-#   default = "orders.copaerp.site"
-# }
+variable "domain_name" {
+  default = "orders.copaerp.site"
+}
 
-# # ----------------------------
-# # S3 Bucket (arquivos buildados)
-# # ----------------------------
-# resource "aws_s3_bucket" "frontend" {
-#   bucket = "copaerp-orders-ui-bucket"
-# }
+resource "aws_s3_bucket" "frontend" {
+  bucket = "copaerp-orders-ui-bucket"
 
-# # Cluster ECS
-# resource "aws_ecs_cluster" "orders" {
-#   name = "orders-cluster"
-# }
+  tags = {
+    Name = "copaerp-orders-ui-bucket"
+  }
+}
 
-# # Task Definition
-# resource "aws_ecs_task_definition" "orders_ui" {
-#   family                   = "orders-ui"
-#   requires_compatibilities = ["FARGATE"]
-#   network_mode             = "awsvpc"
-#   cpu                      = "256"
-#   memory                   = "512"
-#   execution_role_arn       = var.iam_role_id
+resource "aws_s3_bucket_website_configuration" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
 
-#   container_definitions = jsonencode([{
-#     name  = "orders-ui"
-#     image = "nginx:latest"
-#     portMappings = [{
-#       containerPort = 80
-#       hostPort      = 80
-#     }]
-#   }])
-# }
+  index_document {
+    suffix = "index.html"
+  }
 
-# # Service ECS (usa default VPC/subnets automaticamente)
-# resource "aws_ecs_service" "orders_ui" {
-#   name            = "orders-ui"
-#   cluster         = aws_ecs_cluster.orders.id
-#   task_definition = aws_ecs_task_definition.orders_ui.arn
-#   desired_count   = 1
-#   launch_type     = "FARGATE"
+  error_document {
+    key = "index.html"
+  }
+}
 
-#   network_configuration {
-#     assign_public_ip = true
-#     # se não informar subnets, Terraform vai reclamar
-#     # então você pode buscar as subnets default com data sources
-#     subnets = data.aws_subnets.default.ids
-#   }
+resource "aws_s3_bucket_policy" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
 
-#   load_balancer {
-#     target_group_arn = aws_lb_target_group.orders_ui.arn
-#     container_name   = "orders-ui"
-#     container_port   = 80
-#   }
-# }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.frontend.arn}/*"
+      }
+    ]
+  })
+}
 
-# # Descobrir subnets da VPC default
-# data "aws_vpc" "default" {
-#   default = true
-# }
+resource "aws_s3_bucket_public_access_block" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
 
-# data "aws_subnets" "default" {
-#   filter {
-#     name   = "vpc-id"
-#     values = [data.aws_vpc.default.id]
-#   }
-# }
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
 
-# # ALB
-# resource "aws_lb" "orders_ui" {
-#   name               = "orders-ui"
-#   load_balancer_type = "application"
-#   subnets            = data.aws_subnets.default.ids
-# }
+resource "aws_route53_record" "frontend" {
+  zone_id = var.route53_main_zone_id
+  name    = var.domain_name
+  type    = "CNAME"
+  ttl     = 300
 
-# resource "aws_lb_target_group" "orders_ui" {
-#   name        = "orders-ui"
-#   port        = 80
-#   protocol    = "HTTP"
-#   vpc_id      = data.aws_vpc.default.id
-#   target_type = "ip"
-# }
+  records = [aws_s3_bucket_website_configuration.frontend.website_endpoint]
+}
 
-# resource "aws_lb_listener" "orders_ui" {
-#   load_balancer_arn = aws_lb.orders_ui.arn
-#   port              = 80
-#   protocol          = "HTTP"
-
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.orders_ui.arn
-#   }
-# }
-
-# # DNS
-# resource "aws_route53_record" "orders_ui" {
-#   zone_id = var.route53_main_zone_id
-#   name    = var.domain_name
-#   type    = "A"
-
-#   alias {
-#     name                   = aws_lb.orders_ui.dns_name
-#     zone_id                = aws_lb.orders_ui.zone_id
-#     evaluate_target_health = true
-#   }
-# }
+output "website_endpoint" {
+  value = aws_s3_bucket_website_configuration.frontend.website_endpoint
+}
