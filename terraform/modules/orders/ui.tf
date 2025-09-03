c5e073178e8e77dc0c24acf14e0b2373aee3947d
joaqu1m <joaqu1m.pires@hotmail.com>
2025-09-03 22:39:30 +0000

    rebuilding ec2

--- terraform/modules/orders/ui.tf
+++ terraform/modules/orders/ui.tf
@@ -109,48 +109,48 @@ resource "aws_iam_instance_profile" "nginx_ec2_instance_profile" {
   role = var.iam_role_name
 }
 
-# resource "aws_instance" "nginx_ec2" {
-#   ami                  = data.aws_ami.amazon_linux.id
-#   key_name             = var.key_name
-#   instance_type        = "t2.micro"
-#   iam_instance_profile = aws_iam_instance_profile.nginx_ec2_instance_profile.name
+resource "aws_instance" "nginx_ec2" {
+  ami                  = data.aws_ami.amazon_linux.id
+  key_name             = var.key_name
+  instance_type        = "t2.micro"
+  iam_instance_profile = aws_iam_instance_profile.nginx_ec2_instance_profile.name
 
-#   security_groups = [aws_security_group.nginx_sg.name]
+  security_groups = [aws_security_group.nginx_sg.name]
 
-#   root_block_device {
-#     volume_size = 20
-#     volume_type = "gp2"
-#   }
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp2"
+  }
 
-#   user_data = <<-EOF
-# #!/bin/bash
-# yum update -y
-# amazon-linux-extras install -y nginx1
-# systemctl enable nginx
-# systemctl start nginx
+  user_data = <<-EOF
+#!/bin/bash
+yum update -y
+amazon-linux-extras install -y nginx1
+systemctl enable nginx
+systemctl start nginx
 
-# # Configura Nginx para fazer proxy para o bucket
-# cat > /etc/nginx/conf.d/react.conf <<EOC
-# server {
-#     listen 80;
-#     server_name orders.copaerp.site;
+# Configura Nginx para fazer proxy para o bucket
+cat > /etc/nginx/conf.d/react.conf <<EOC
+server {
+    listen 80;
+    server_name orders.copaerp.site;
 
-#     location / {
-#         proxy_pass http://copaerp-orders-ui-bucket.s3-website-us-east-1.amazonaws.com/;
-#         proxy_set_header Host \$host;
-#         proxy_set_header X-Real-IP \$remote_addr;
-#     }
-# }
-# EOC
+    location / {
+        proxy_pass http://copaerp-orders-ui-bucket.s3-website-us-east-1.amazonaws.com/;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+    }
+}
+EOC
 
-# systemctl restart nginx
-#               EOF
+systemctl restart nginx
+              EOF
 
-#   tags = {
-#     Name = "nginx-ec2"
-#   }
-# }
+  tags = {
+    Name = "nginx-ec2"
+  }
+}
 
-# output "ec2_public_ip" {
-#   value = aws_instance.nginx_ec2.public_ip
-# }
+output "ec2_public_ip" {
+  value = aws_instance.nginx_ec2.public_ip
+}
