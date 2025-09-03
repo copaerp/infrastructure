b678d2d30dc298a4edade0655719391a0eb2bdee
joaqu1m <joaqu1m.pires@hotmail.com>
2025-09-03 23:05:13 +0000

    new r53 registry

--- terraform/modules/route53/main.tf
+++ terraform/modules/route53/main.tf
@@ -18,11 +18,6 @@ resource "aws_route53_zone" "copaerp_site" {
   }
 }
 
-output "copaerp_main_zone_id" {
-  value = aws_route53_zone.copaerp_site.zone_id
-  
-}
-
 resource "aws_route53_record" "n8n_copaerp_site" {
   zone_id = aws_route53_zone.copaerp_site.zone_id
   name    = "n8n.copaerp.site"
@@ -31,6 +26,18 @@ resource "aws_route53_record" "n8n_copaerp_site" {
   records = [var.n8n_elastic_ip]
 }
 
+resource "aws_route53_record" "n8n_copaerp_site" {
+  zone_id = aws_route53_zone.copaerp_site.zone_id
+  name    = "orders.copaerp.site"
+  type    = "A"
+  ttl     = "300"
+  records = [var.n8n_elastic_ip]
+}
+
 output "copaerp_site_zone_id" {
+  value = aws_route53_zone.copaerp_site.zone_id
+}
+
+output "copaerp_main_zone_id" {
   value = aws_route53_zone.copaerp_site.zone_id
 }
