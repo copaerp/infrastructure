5ba9caf20a0cd5bad5e1ce38235522d9ef39e66f
joaqu1m <joaqu1m.pires@hotmail.com>
2025-11-10 00:13:45 +0000

    trying to solve cors prob

--- terraform/modules/orders/lambda-fb.tf
+++ terraform/modules/orders/lambda-fb.tf
@@ -3,11 +3,12 @@ resource "aws_apigatewayv2_api" "frontend_bridge_api" {
   protocol_type = "HTTP"
 
   cors_configuration {
-    allow_origins  = ["*"]
-    allow_methods  = ["*"]
-    allow_headers  = ["*"]
-    expose_headers = ["*"]
-    max_age        = 3600
+    allow_origins     = ["*"]
+    allow_methods     = ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"]
+    allow_headers     = ["Content-Type", "Authorization", "X-Amz-Date", "X-Api-Key", "X-Amz-Security-Token", "X-Amz-User-Agent"]
+    expose_headers    = ["Content-Length", "Content-Type"]
+    allow_credentials = false
+    max_age           = 3600
   }
 }
 
@@ -18,10 +19,9 @@ resource "aws_apigatewayv2_stage" "frontend_bridge_stage" {
 }
 
 resource "aws_apigatewayv2_integration" "frontend_bridge_integration" {
-  api_id             = aws_apigatewayv2_api.frontend_bridge_api.id
-  integration_type   = "AWS_PROXY"
-  integration_uri    = aws_lambda_function.frontend_bridge.invoke_arn
-  integration_method = "POST"
+  api_id           = aws_apigatewayv2_api.frontend_bridge_api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.frontend_bridge.invoke_arn
 }
 
 resource "aws_apigatewayv2_route" "frontend_bridge_route" {
